015cd1ea8f2ee48a5527ad63dd15769e255422f5
Jeremy Maslanko <maslankoj@gmail.com>
2024-12-03 09:41:42 -0500

    msg

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -2,25 +2,4 @@
 
 left_list = []
 right_list = []
-with open("/Users/Jeremy/Documents/GitHub/AdventOfCode/2024/day1/data.txt", "r") as f:
-    for line in f:
-        output = line.split()
-        left_list.append(int(output[0]))
-        right_list.append(int(output[1]))
-
-left_list.sort()
-right_list.sort()
-
-total_distance = 0
-for i in range(0, len(left_list)):
-
-    diff = abs(left_list[i] - right_list[i])
-    total_distance += diff
-
-# Part 2
-
-total_score = 0
-for i in left_list:
-    right_list_cnt = right_list.count(i)
-    similarity_score = right_list_cnt*i
-    total_score += similarity_score
+with open("/Users/Jeremy/Documents/GitHub/AdventOfCode/2024/day1/data.txt", "r
